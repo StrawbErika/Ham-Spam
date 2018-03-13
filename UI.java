81ecccc8121e3468ba51aab750f1eab1994e278a
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-03-13 22:37:18 +0800

    Added comments

--- UI.java
+++ UI.java
@@ -8,8 +8,10 @@ import javax.imageio.*;
 
 public class UI {
     private JFrame frame;
+    public HashMap<String, ArrayList> classifyDictionary;
 
     public UI() {
+        classifyDictionary = new HashMap<String, ArrayList>();
         String s="";
         BagOfWords spam = new BagOfWords();
         for(int i = 1; i<300; i++){
@@ -29,10 +31,9 @@ public class UI {
         System.out.println("ham lines " + ham.numberOfLines);
         ham.saveFile(ham.dictionarySize, ham.numberOfWords);
 
-        BagOfWords classify = new BagOfWords();
-
         s = String.format("all_data/classify/001");
-        classifyFile(s);
+        //loads all files in classify (atm 1 muna) and saves it in the dictionary
+        classifyLoadFile(s, classifyDictionary);
 
         Probability p = new Probability();
         double pMessageSpam = 1;
@@ -40,43 +41,68 @@ public class UI {
         double pSpam = p.pSpam(spam.numberOfWords, ham.numberOfWords);
         double pHam = p.pHam(pSpam);
 
-        for (Map.Entry<String, Integer> entry : spam.dictionary.entrySet()) {
-                double pWordSpam = p.pWordSpam(spam.numberOfWords, entry.getValue());
-                pMessageSpam = pWordSpam * pMessageSpam;
+        //gets pMessageSpam & pMessageHam by looping through the dictionary and saves each probability in a file
+        for (Map.Entry<String, ArrayList> entry : classifyDictionary.entrySet()) {
+          double sVal;
+          double hVal;
+          double pWordSpam;
+          double pWordHam;
+          //loops through the array of words of each file (which is saved as the value of each key <file>)
+          for(int i = 0; i < entry.getValue().size(); i++){
+            //gets MessageSpam & messageHam by getting the WordSpam (u get it by getting the #of occurences it is in Spam/Ham)
+            if(spam.dictionary.containsKey(entry.getValue().get(i))){
+                sVal = spam.dictionary.get(entry.getValue().get(i));
+            }else{
+              sVal = 0;
+            }
+            pWordSpam = p.pWordSpam(spam.numberOfWords, sVal);
+            pMessageSpam = pWordSpam * pMessageSpam;
+
+            if(ham.dictionary.containsKey(entry.getValue().get(i))){
+                hVal = ham.dictionary.get(entry.getValue().get(i));
+            }else{
+              hVal = 0;
+            }
+            pWordHam = p.pWordHam(spam.numberOfWords, hVal);
+            pMessageHam = pWordHam * pMessageHam;
+
+          }
+
+          System.out.println(pMessageHam);
+          double pMessage = p.pMessage(pMessageSpam, pSpam, pMessageHam, pHam);
+          System.out.println(pMessage);
+          double pSpamMessage = p.pSpamMessage(pMessageSpam, pMessage);
+          double pHamMessage = p.pHamMessage(pMessageHam, pMessage);
+          System.out.println(pSpamMessage);
+          System.out.println(pHamMessage);
+
+          classifyFile(entry.getKey(), "Ham", pSpamMessage);
         }
 
-        for (Map.Entry<String, Integer> entry : ham.dictionary.entrySet()) {
-                double pWordHam = p.pWordHam(ham.numberOfWords, entry.getValue());
-                pMessageHam = pWordHam * pMessageHam;
-        }
-
-
-        double pMessage = p.pMessage(pMessageSpam, pSpam, pMessageHam, pHam);
-        double pSpamMessage = p.pSpamMessage(pMessageSpam, pMessage);
-        System.out.println(pSpamMessage);
 
 
         this.initializeUI();
     }
 
 
-    public void loadFile(String filename) {
+    public void classifyLoadFile(String filename, HashMap dictionary) {
         try{
             FileInputStream fstream = new FileInputStream(filename);
             DataInputStream in = new DataInputStream(fstream);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String line;
 
+            ArrayList<String> list = new ArrayList<String>();
             while((line  = br.readLine()) != null)
             {
                 String[] values = line.split(" "); //stores all the words from the line in values
                 for (String str : values) {
                     str = str.toLowerCase();
                     str = str.replaceAll("[^a-zA-Z0-9]", ""); //checks if none alphanumeric
-
-
+                    list.add(str);
                 }
             }
+            dictionary.put(filename, list); //stores str in dictionary
             in.close();
         } catch (Exception e) {//Catch exception if any
             System.err.println("Error: " + e.getMessage());
@@ -84,7 +110,7 @@ public class UI {
     }
 
 
-    public void classifyFile(String number, String name, int probabilitySpam) { //writes file
+    public void classifyFile(String number, String name, double probabilitySpam) { //writes file
         String filename = "classify.out";
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
