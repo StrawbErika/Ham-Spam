b53dad91bc73ead7afa5ab6f1a2f26c5930f186d
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-03-16 08:10:24 +0800

    + added total words, + whole classify panel works, *SpamWords still derpy

--- UI.java
+++ UI.java
@@ -9,6 +9,7 @@ import javax.imageio.*;
 public class UI {
     private JFrame frame;
     public HashMap<String, ArrayList> classifyDictionary;
+    public HashMap<String, ArrayList> outputDictionary;
     public int numberOfSpamMsgs;
     public int numberOfHamMsgs;
     public int numberOfClassifyMsgs;
@@ -16,6 +17,7 @@ public class UI {
     public BagOfWords ham;
     public UI() {
         this.classifyDictionary = new HashMap<String, ArrayList>();
+        this.outputDictionary = new HashMap<String, ArrayList>();
         this.spam = new BagOfWords();
         this.ham = new BagOfWords();
         String s="";
@@ -89,12 +91,20 @@ public class UI {
             pSpamMessage = 0;
           }
 
+          System.out.println(entry.getKey()+" : "+pSpamMessage);
+          String classify = " ";
           if (pSpamMessage > 0.5){
-            classifyFile(entry.getKey(), "Spam", pSpamMessage);
+            classify = "Spam";
+            classifyFile(entry.getKey(), classify, pSpamMessage);
           }
           else{
-            classifyFile(entry.getKey(), "Ham", pSpamMessage);
+            classify = "Ham";
+            classifyFile(entry.getKey(), classify , pSpamMessage);
           }
+          ArrayList<String> output = new ArrayList<String>();
+          output.add(classify);
+          output.add(Double.toString(pSpamMessage));
+          outputDictionary.put(entry.getKey(), output);
         }
     }
 
@@ -166,7 +176,7 @@ public class UI {
       JLabel stats02= new JLabel();
       JFrame frame = new JFrame("Ham & Spam");//creates a frame/window; javax.swing.JFrame
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setPreferredSize(new Dimension(1000, 700));//sets initial size resolution of the frame...; java.awt.Dimensio
+      frame.setPreferredSize(new Dimension(1200, 700));//sets initial size resolution of the frame...; java.awt.Dimensio
 
       Container con = frame.getContentPane();
       JPanel contentPanel= new JPanel();
@@ -174,34 +184,48 @@ public class UI {
       JPanel tabPanel= new JPanel();
       tabPanel.setLayout(new FlowLayout());
       tabPanel.setPreferredSize(new Dimension(250,500));
-      tabPanel.setBackground(Color.WHITE);
+      // tabPanel.setBackground(Color.WHITE);
 
       JTextArea wordsSpam = new JTextArea();
       JTextArea frequenciesSpam = new JTextArea();
       JButton information= new JButton("Select Spam Folder here");
+      JPanel totalWordSpam = new JPanel();
+      totalWordSpam.setLayout(new GridLayout(1,2));
+      JLabel totalSpam = new JLabel("Total words in Spam: ");
+      JTextArea totalS = new JTextArea();
+      totalWordSpam.add(totalSpam);
+      totalWordSpam.add(totalS);
+
       information.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent e){
           JFileChooser fileChooser = new JFileChooser();
           int result = fileChooser.showOpenDialog(frame);
           if (result == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = fileChooser.getSelectedFile();
-            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-            spam.loadFile(selectedFile.getAbsolutePath());
-            spam.saveFile(spam.dictionarySize, spam.numberOfWords, "outputHam.txt");
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            String a = "";
+            String s = "";
+            String fileDirectory = (fileChooser.getCurrentDirectory()).getPath();
+            int numberOfSpamMsgs = new File(fileDirectory).listFiles().length;
+            for(int i = 1; i<=numberOfSpamMsgs; i++){
+                a = String.format("%03d", i);
+                s = String.format(fileDirectory+"/" + a);
+                spam.loadFile(s);
+            }
             for (Map.Entry<String, Integer> entry : spam.dictionary.entrySet()) {
-              wordsSpam.append("                                  "+ entry.getKey()+ '\n');
+              wordsSpam.append(entry.getKey()+ '\n');
               frequenciesSpam.append("             "+ entry.getValue()+ '\n');
             }
+            totalS.append(Integer.toString(spam.numberOfWords));
           }
           frame.requestFocus();
-        }
+          }
       });
 
       information.setPreferredSize(new Dimension(200,50));
 
       JPanel wordFrequencySpamPanel= new JPanel();
       wordFrequencySpamPanel.setLayout(new GridLayout(1,2));
-      wordFrequencySpamPanel.setPreferredSize(new Dimension(300,700));
+      wordFrequencySpamPanel.setPreferredSize(new Dimension(300,550));
 
       JPanel wordSpamPanel= new JPanel();
       wordSpamPanel.setBackground(Color.WHITE);
@@ -229,6 +253,7 @@ public class UI {
 
       tabPanel.add(information);
       tabPanel.add(wordFrequencySpamPanel);
+      tabPanel.add(totalWordSpam);
 
 
       contentPanel.setLayout(new CardLayout());
@@ -237,25 +262,38 @@ public class UI {
       //panel 1
       JPanel hamPanel= new JPanel();
       hamPanel.setLayout(new FlowLayout());
-      hamPanel.setPreferredSize(new Dimension(250,500));
-      hamPanel.setBackground(Color.WHITE);
+      hamPanel.setPreferredSize(new Dimension(250,550));
 
       JButton panel1= new JButton("Select Ham Folder here");
       JTextArea words = new JTextArea();
       JTextArea frequencies = new JTextArea();
+      JPanel totalWordHam = new JPanel();
+      totalWordHam.setLayout(new GridLayout(1,2));
+      JLabel totalHam = new JLabel("Total words in Ham: ");
+      JTextArea totalH = new JTextArea();
+      totalWordHam.add(totalHam);
+      totalWordHam.add(totalH);
       panel1.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent e){
           JFileChooser fileChooser = new JFileChooser();
           int result = fileChooser.showOpenDialog(frame);
           if (result == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = fileChooser.getSelectedFile();
-            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-            ham.loadFile(selectedFile.getAbsolutePath());
-            ham.saveFile(ham.dictionarySize, ham.numberOfWords, "outputHam.txt");
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            System.out.println(fileChooser.getCurrentDirectory());
+            String a = "";
+            String s = "";
+            String fileDirectory = (fileChooser.getCurrentDirectory()).getPath();
+            int numberOfHamMsgs = new File(fileDirectory).listFiles().length;
+            for(int i = 1; i<=numberOfHamMsgs; i++){
+                a = String.format("%03d", i);
+                s = String.format(fileDirectory+"/" + a);
+                ham.loadFile(s);
+            }
             for (Map.Entry<String, Integer> entry : ham.dictionary.entrySet()) {
-              words.append("                                  "+ entry.getKey()+ '\n');
+              words.append("                                                                                                                 "+ entry.getKey()+ '\n');
               frequencies.append("             "+ entry.getValue()+ '\n');
             }
+            totalH.append(Integer.toString(ham.numberOfWords));
           }
           frame.requestFocus();
         }
@@ -265,8 +303,8 @@ public class UI {
 
       JPanel wordFrequencyPanel= new JPanel();
       wordFrequencyPanel.setLayout(new GridLayout(1,2));
-      wordFrequencyPanel.setPreferredSize(new Dimension(300,700));
-
+      wordFrequencyPanel.setPreferredSize(new Dimension(300,550));
+      wordFrequencyPanel.setBackground(Color.GRAY);
       JPanel wordPanel= new JPanel();
       wordPanel.setBackground(Color.WHITE);
       JPanel textWPanel= new JPanel();
@@ -292,18 +330,25 @@ public class UI {
 
       hamPanel.add(panel1);
       hamPanel.add(wordFrequencyPanel);
+      hamPanel.add(totalWordHam);
 
 
   //panel 2
       JPanel panel02= new JPanel();
+      panel02.setPreferredSize(new Dimension(600,60));
       JPanel dictionaryTotal = new JPanel();
-      dictionaryTotal.setPreferredSize(new Dimension(350,60));
+      dictionaryTotal.setPreferredSize(new Dimension(500,60));
       dictionaryTotal.setBackground(Color.WHITE);
-      dictionaryTotal.setLayout(new GridLayout(1, 2));
-      JLabel dictionarySize = new JLabel("   Dictionary Size:");
+      dictionaryTotal.setLayout(new GridLayout(1, 4));
+      JTextArea dSize = new JTextArea();
+      dSize.setPreferredSize(new Dimension(10,60));
+      JTextArea tWords = new JTextArea();
+      JLabel dictionarySize = new JLabel("               D_Size:");
       JLabel totalWords = new JLabel("Total Words: ");
       dictionaryTotal.add(dictionarySize);
+      dictionaryTotal.add(dSize);
       dictionaryTotal.add(totalWords);
+      dictionaryTotal.add(tWords);
 
       JPanel buttonsPanel = new JPanel();
       buttonsPanel.setPreferredSize(new Dimension(350,100));
@@ -324,7 +369,9 @@ public class UI {
       JPanel fileContent = new JPanel();
       fileContent.setPreferredSize(new Dimension(200,500));
       fileContent.setBackground(Color.WHITE);
+      JTextArea fileC = new JTextArea();
       fileName.add(file);
+      fileContent.add(fileC);
       filePanel.add(fileName);
       filePanel.add(fileContent);
 
@@ -335,7 +382,9 @@ public class UI {
       JLabel classN = new JLabel("Class");
       JPanel classContent = new JPanel();
       classContent.setPreferredSize(new Dimension(200,500));
-      classContent.setBackground(Color.PINK);
+      classContent.setBackground(Color.WHITE);
+      JTextArea classC = new JTextArea();
+      classContent.add(classC);
       className.add(classN);
       classPanel.add(className);
       classPanel.add(classContent);
@@ -344,11 +393,14 @@ public class UI {
       JPanel spamName = new JPanel();
       spamName.setPreferredSize(new Dimension(300, 30));
       spamName.setBackground(Color.LIGHT_GRAY);
+
       JLabel spamN = new JLabel("pSpam");
       JPanel spamContent = new JPanel();
       spamContent.setPreferredSize(new Dimension(200,500));
       spamContent.setBackground(Color.WHITE);
+      JTextArea spamC = new JTextArea();
       spamName.add(spamN);
+      spamContent.add(spamC);
       pSpamPanel.add(spamName);
       pSpamPanel.add(spamContent);
 
@@ -360,9 +412,19 @@ public class UI {
           JFileChooser fileChooser = new JFileChooser();
           int result = fileChooser.showOpenDialog(frame);
           if (result == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = fileChooser.getSelectedFile();
-            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            System.out.println(fileChooser.getCurrentDirectory());
+            String a = "";
+            String s = "";
+            String fileDirectory = (fileChooser.getCurrentDirectory()).getPath();
+            int numberOfClassifyMsgs = new File(fileDirectory).listFiles().length;
+            for(int i = 1; i<=numberOfClassifyMsgs; i++){
+                a = String.format("%03d", i);
+                s = String.format("all_data/classify/" + a);
+                classifyLoadFile(a, classifyDictionary);
+            }
+            dSize.append(Integer.toString(ham.dictionarySize + spam.dictionarySize));
+            tWords.append(Integer.toString(ham.numberOfWords + spam.dictionarySize));
           }
           frame.requestFocus();
         }
@@ -370,6 +432,13 @@ public class UI {
 
       filter.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent e){
+          findSpam();
+          for (Map.Entry<String, ArrayList> entry : outputDictionary.entrySet()) {
+            fileC.append(entry.getKey()+ '\n');
+            ArrayList<String> value = entry.getValue();
+            classC.append(value.get(0)+ '\n');
+            spamC.append("                            "+value.get(1)+ '\n');
+          }
         }
       });
 
